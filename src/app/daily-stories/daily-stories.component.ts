8315e93256c1c136a82d85eab7f70c6466a3604d
Hayyan <hayyan@chb-technologies.ch>
2024-07-07 14:54:17 +0400

    dailystories component

---
+++ src/app/daily-stories/daily-stories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-stories',
+  standalone: true,
+  imports: [],
+  templateUrl: './daily-stories.component.html',
+  styleUrl: './daily-stories.component.css'
+})
+export class DailyStoriesComponent {
+
+}
