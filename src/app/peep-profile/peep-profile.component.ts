17067747c2f4da5f8272000cc318a23c37d19132
Hayyan <hayyan@chb-technologies.ch>
2024-08-07 16:51:31 +0400

    people in my life is ok

--- src/app/peep-profile/peep-profile.component.ts
+++ src/app/peep-profile/peep-profile.component.ts
@@ -1,11 +1,14 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { SharingService } from '../services/sharing.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { HttpService } from '../services/http.service';
+import { DatePipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-peep-profile',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './peep-profile.component.html',
   styleUrls: ['../chat/chat.component.css', './peep-profile.component.css'],
 })
@@ -13,13 +16,40 @@ export class PeepProfileComponent implements OnInit {
   sharingService = inject(SharingService);
   destroyRef = inject(DestroyRef);
   peep: any = {};
+  httpService = inject(HttpService);
+  router = inject(Router);
+  dayChats: any = [];
   ngOnInit(): void {
-    this.sharingService.peepToView$
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((peep: any) => {
-        this.peep = peep;
-        console.log(this.peep);
-      });
+    this.httpService.getAllDayChats().subscribe((resp: any) => {
+      this.dayChats = resp;
+      console.log(this.dayChats);
+      this.sharingService.peepToView$
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((peep: any) => {
+          const people = peep;
+          console.log(people);
+          people.dayChats = [];
+          people.mentions.forEach((mention: any) => {
+            console.log(mention);
+            const dayChat = this.dayChats.find(
+              (dayChat: any) => dayChat._id === mention.trim()
+            );
+            console.log(dayChat);
+
+            if (dayChat) {
+              people.dayChats.push(dayChat);
+            }
+          });
+          console.log(people);
+          this.peep = people;
+        });
+    });
+  }
+
+  goToStory(story: any) {
+    console.log(story);
+    this.sharingService.updateDayToGenerate(story);
+    this.router.navigateByUrl('story/' + story._id);
   }
 
   toggleSidenav() {
