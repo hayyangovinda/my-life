56eb4aafd0cf107253e0e43caa97edb13f862948
Hayyan <hayyan@chb-technologies.ch>
2024-08-07 18:53:20 +0400

    emoji picker working fine

--- src/app/group-form/group-form.component.ts
+++ src/app/group-form/group-form.component.ts
@@ -1,28 +1,49 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { SharingService } from '../services/sharing.service';
+import { NgIf } from '@angular/common';
+import { PickerComponent } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-group-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, PickerComponent, NgIf],
   templateUrl: './group-form.component.html',
   styleUrls: ['../chat/chat.component.css', './group-form.component.css'],
 })
 export class GroupFormComponent {
+  showPicker = false;
   onSaveClick() {
     throw new Error('Method not implemented.');
   }
   sharingService = inject(SharingService);
 
+  groupForm = new FormGroup({
+    icon: new FormControl(''),
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
+
   toggleSidenav() {
     this.sharingService.toggleSidenav();
   }
-  onIconFocus($event: FocusEvent) {
-    throw new Error('Method not implemented.');
-  }
+
   categoryForm: any;
   onBackClick() {
     throw new Error('Method not implemented.');
+  }
+
+  emojiClick(event: any) {
+    this.categoryForm.get('icon')?.setValue(event.emoji.native);
+    this.showPicker = false;
+  }
+
+  onOverlayClick() {
+    this.showPicker = false;
+  }
+
+  onIconFocus(event: any) {
+    event.preventDefault();
+    this.showPicker = true;
   }
 }
