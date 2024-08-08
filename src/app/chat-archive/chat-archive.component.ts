7882465fc270f3be43021134b26c4d4819965727
Hayyan <hayyan@chb-technologies.ch>
2024-08-08 15:21:44 +0400

    chat archive ok

--- src/app/chat-archive/chat-archive.component.ts
+++ src/app/chat-archive/chat-archive.component.ts
@@ -1,16 +1,64 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SharingService } from '../services/sharing.service';
+import { DatePipe } from '@angular/common';
+import { MatFormField } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { startOfYear, endOfYear } from 'date-fns';
+import { HttpService } from '../services/http.service';
+import {
+  provideNativeDateAdapter,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
 
 @Component({
   selector: 'app-chat-archive',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe,
+    MatFormField,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './chat-archive.component.html',
-  styleUrls: ['../chat/chat.component.css', './chat-archive.component.css'],
+  styleUrls: [
+    '../chat/chat.component.css',
+    '../daily-stories/daily-stories.component.css',
+    './chat-archive.component.css',
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  ],
 })
-export class ChatArchiveComponent {
+export class ChatArchiveComponent implements OnInit {
+  currentYear = new Date();
+  firstDayOfYear = startOfYear(this.currentYear);
+  lastDayOfYear = endOfYear(this.currentYear);
+  range = new FormGroup({
+    start: new FormControl<Date | null>(this.firstDayOfYear),
+    end: new FormControl<Date | null>(this.lastDayOfYear),
+  });
   sharingService = inject(SharingService);
+  httpService = inject(HttpService);
+  chats: any = [];
+
+  ngOnInit(): void {
+    this.httpService.getAllDayChats().subscribe((response: any) => {
+      this.chats = response;
+      console.log(this.chats);
+    });
+  }
+
   toggleSidenav() {
     this.sharingService.toggleSidenav();
   }
+
+  goToChat(chat: any) {}
 }
