aa1873b274ed9e58ed6c3d0f11db3a8ea88015f9
Hayyan <hayyan@chb-technologies.ch>
2024-08-09 12:05:05 +0400

    login ui touch up

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.mylife.app',
   appName: 'my-life',
   webDir: 'dist/my-life/browser',
-  server: {
-    url: 'https://my-life-78999.web.app',
-  },
 };
 
 export default config;
--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -44,4 +44,8 @@ export class ForgotPasswordComponent {
         }
       );
   }
+
+  onBackClick() {
+    this.router.navigateByUrl('login');
+  }
 }
